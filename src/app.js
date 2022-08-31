1c394662fdb6078d97a598f9011d7cc960bd66c8
DESKTOP-C60BUTI\Wasp <paulinefugit@gmail.com>
2022-08-31 18:26:41 -0400

    cardgenerator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const card = document.querySelector(".card");
+const numb = document.querySelector(".numb");
+const btn = document.querySelector(".btn");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateRandomNumber = () => {
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  const indexNumb = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumb];
 };
+
+const generateRandomSimb = () => {
+  const simb = ["hearts", "diamonds", "spades", "clubs"];
+  const indexSimb = Math.floor(Math.random() * simb.length);
+  return simb[indexSimb];
+};
+
+window.onload = () => {
+  card.classList.add(generateRandomSimb());
+  numb.innerHTML = generateRandomNumber();
+};
+
+btn.addEventListener("click", e => {
+  card.classList.remove(generateRandomSimb());
+  card.classList.add(generateRandomSimb());
+  numb.innerHTML = generateRandomNumber();
+});
